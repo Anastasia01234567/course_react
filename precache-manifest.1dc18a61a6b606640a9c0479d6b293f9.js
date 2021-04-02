e3fca9f1695bad2146828a7430e764688143d477
Anastasia01234567 <nastyaushko20@gmail.com>
2021-04-02 21:03:06 +0300

    Updates

---
+++ precache-manifest.1dc18a61a6b606640a9c0479d6b293f9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2469476b48f66e82e73d48271027d6",
+    "url": "//anastasia01234567.github.io/course_react/index.html"
+  },
+  {
+    "revision": "627853f54eb97298c423",
+    "url": "//anastasia01234567.github.io/course_react/static/css/3.16841c85.chunk.css"
+  },
+  {
+    "revision": "4fab0b3312fee5371b5f",
+    "url": "//anastasia01234567.github.io/course_react/static/css/4.016a5ba6.chunk.css"
+  },
+  {
+    "revision": "f1cb6dc03857a026a337",
+    "url": "//anastasia01234567.github.io/course_react/static/css/main.39f67f96.chunk.css"
+  },
+  {
+    "revision": "0cc31a2dd41e7626f6d7",
+    "url": "//anastasia01234567.github.io/course_react/static/js/2.ecda0db4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "//anastasia01234567.github.io/course_react/static/js/2.ecda0db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627853f54eb97298c423",
+    "url": "//anastasia01234567.github.io/course_react/static/js/3.80576168.chunk.js"
+  },
+  {
+    "revision": "4fab0b3312fee5371b5f",
+    "url": "//anastasia01234567.github.io/course_react/static/js/4.7c52815b.chunk.js"
+  },
+  {
+    "revision": "f1cb6dc03857a026a337",
+    "url": "//anastasia01234567.github.io/course_react/static/js/main.3f0902c0.chunk.js"
+  },
+  {
+    "revision": "43372d2981d5362dc6ea",
+    "url": "//anastasia01234567.github.io/course_react/static/js/runtime-main.36f0f50a.js"
+  }
+]);
