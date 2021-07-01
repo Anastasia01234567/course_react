cdf21c18a86d06cde5e862215f432a89219a4044
Anastasia <nastyaushko20@gmail.com>
2021-07-01 10:43:21 +0300

    hook

--- 01-first-project/react-project-1/src/hoc/withAuthRedirect.tsx
+++ 01-first-project/react-project-1/src/hoc/withAuthRedirect.tsx
@@ -6,12 +6,12 @@ let mapStateToPropsRedirect = (state) =>{
         isAuth: state.auth.isAuth,
         id: state.auth.id
     }
-}
+};
 export const withAuthRedirect = (Component) => {
     let RedirectComponent = (props) =>{
         if(!props.isAuth) return <Redirect to={"/login"}/>;
         return <Component {...props}/>
-    }
+    };
     // class RedirectComponent extends React.Component {
     //     render() {
     //         if(!this.props.isAuth) return <Redirect to={"/login"}/>
@@ -21,4 +21,4 @@ export const withAuthRedirect = (Component) => {
 
     let ConnectAuthRedirectComponent = connect(mapStateToPropsRedirect)(RedirectComponent);
     return connect(mapStateToPropsRedirect)(RedirectComponent);
-}
+};
